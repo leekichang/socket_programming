cfa933d3f6b1112ba4f0a7ecdf0e8e1d6096e374
leekichang <kichang.lee@yonsei.ac.kr>
2023-03-20 13:55:15 +0900

    pickle size upgraded

--- client.py
+++ client.py
@@ -30,9 +30,9 @@ class Client:
     def recv(self):
         recv_data = []
         while True:
-            chunk = self.socket.recv(1024)
+            chunk = self.socket.recv(4096)
             recv_data.append(chunk)
-            if len(chunk) < 1024:
+            if len(chunk) < 4096:
                 break
         if not recv_data:
             print('no receive data')
--- server.py
+++ server.py
@@ -35,9 +35,9 @@ class Server:
     def recv(self):
         recv_data = []
         while True:
-            chunk = self.client_socket.recv(1024)
+            chunk = self.client_socket.recv(4096)
             recv_data.append(chunk)
-            if len(chunk) < 1024:
+            if len(chunk) < 4096:
                 break
         # recv_data = self.client_socket.recv(4096)
         data = pickle.loads(b''.join(recv_data))
